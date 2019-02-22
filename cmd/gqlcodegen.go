013149211288cd23031e584c8dc9ff5af1ed3512
Yuta Sakata <yusaka39@gmail.com>
2019-02-23 01:33:06 +0900

    Implement cmd entrypoint

--- cmd/gqlcodegen.go
+++ cmd/gqlcodegen.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"flag"
-	"fmt"
 	"github.com/RettyInc/gqlcodegen/ast"
 	"github.com/RettyInc/gqlcodegen/internal/generator"
 	"github.com/RettyInc/gqlcodegen/parser"
@@ -15,65 +14,93 @@ import (
 )
 
 var (
+	fileSuffix = flag.String("suffix", "_gql", "")
 	enumPackagePrefix = flag.String("enum-pkg-prefix", "", "")
 	scalarPackage = flag.String("scalar-pkg", "", "")
 	generateTarget = flag.String("target", "", "comma separated")
 	schema = flag.String("schema", "", "comma separated")
 )
 
-func main() {
-	flag.Parse()
-	fmt.Println(*enumPackagePrefix)
-	fmt.Println(*generateTarget)
+func loadPackage(pattern string) *packages.Package {
 	cfg := &packages.Config{
 		Mode: packages.LoadSyntax,
 		Tests: false,
 	}
-	args := flag.Args()
-	if len(args) == 0 {
-		args = []string{
-			".",
-		}
-	} else {
-		args = []string{
-			path.Dir(args[0]),
-		}
+	pkgs, e := packages.Load(cfg, pattern)
+	if e != nil {
+		log.Fatal(e)
 	}
-	pkgs, _ := packages.Load(cfg, args...)
-	pkg := pkgs[0]
+	return pkgs[0]
+}
 
-	ast := loadAst(*schema)
-
-	scalar := map[string]struct{}{}
-	for _, s := range ast.Scalars() {
-		scalar[s.Name()] = struct{}{}
-	}
-
-	enum := map[string]struct{}{}
-	for _, e := range ast.Enums() {
-		enum[e.Name()] = struct{}{}
-	}
-
-	resolver := map[string]struct{}{}
-	for _, r := range ast.Types() {
-		resolver[r.Name()] = struct{}{}
-	}
-	fmt.Printf("%v", resolver)
-
-	genCfg := &generator.Config{
-		ScalarTypes: scalar,
-		ResolverTypes: resolver,
-		EnumTypes:enum,
-		EnumPackagePrefix: *enumPackagePrefix,
-		ScalarPackage: *scalarPackage,
+func createGenerator(pkg *packages.Package, root *ast.Root) *generator.Generator {
+	conf := &generator.Config{
+		EnumTypes:enumerateEnums(root),
+		ScalarTypes:enumerateScalars(root),
+		ResolverTypes:enumerateResolverTypes(root),
+		EnumPackagePrefix:*enumPackagePrefix,
+		ScalarPackage:*scalarPackage,
 		Package:&generator.Package{
-			Name:pkg.Name,
+			Name: pkg.Name,
+			Path: pkg.PkgPath,
 		},
 	}
+	return generator.NewGenerator(conf)
+}
 
-	gen := generator.NewGenerator(genCfg)
-	for _, e := range ast.Types() {
-		writeType(gen, e)
+func enumerateScalars(root *ast.Root) map[string]struct{} {
+	scalar := map[string]struct{}{}
+	for _, s := range root.Scalars() {
+		scalar[s.Name()] = struct{}{}
+	}
+	return scalar
+}
+
+func enumerateEnums(root *ast.Root) map[string]struct{} {
+	enum := map[string]struct{}{}
+	for _, e := range root.Enums() {
+		enum[e.Name()] = struct{}{}
+	}
+	return enum
+}
+
+func enumerateResolverTypes(root *ast.Root) map[string]struct{} {
+	resolver := map[string]struct{}{}
+	for _, r := range root.Types() {
+		resolver[r.Name()] = struct{}{}
+	}
+	return resolver
+}
+
+func main() {
+	flag.Parse()
+
+	pattern := "."
+	if args := flag.Args(); len(args) > 0 {
+		pattern = path.Dir(args[0])
+	}
+	pkg := loadPackage(pattern)
+	rootAst := loadAst(*schema)
+	generator := createGenerator(pkg, rootAst)
+
+	generate(generator, rootAst)
+}
+
+func generate(g *generator.Generator, root *ast.Root) {
+	targets := strings.Split(*generateTarget, ",")
+	for _, t := range targets {
+		switch t {
+		case "enum":
+			for _, e := range root.Enums() {
+				writeEnum(g, e)
+			}
+		case "resolver":
+			for _, t := range root.Types() {
+				writeType(g, t)
+			}
+		default:
+			log.Fatal("unknown target %s", t)
+		}
 	}
 }
 
@@ -81,22 +108,29 @@ func writeEnum(g *generator.Generator,def *ast.EnumDef) {
 	g.GenerateSource(def)
 	defer g.ClearBuff()
 	g.Format()
-	dirName := "./" + strings.ToLower(def.Name())
-	_ = os.Mkdir(dirName, 0755)
-	g.WriteToFile(dirName + "/" + strings.ToLower(def.Name()) + ".go")
+	dirName := path.Join(g.Config().Package.Path, strings.ToLower(def.Name()))
+	e := os.Mkdir(dirName, 0755)
+	if e != nil {
+		log.Fatalf("could not create directory %v", e)
+	}
+	g.WriteToFile(
+		path.Join(dirName, strings.ToLower(def.Name()) + *fileSuffix + ".go"),
+	)
 }
 
 func writeType(g *generator.Generator, def *ast.TypeDef) {
 	g.GenerateSource(def)
 	defer g.ClearBuff()
 	g.Format()
-	g.WriteToFile(strings.ToLower(def.Name()) + ".go")
+	g.WriteToFile(
+		path.Join(g.Config().Package.Path, strings.ToLower(def.Name()) + ".go"),
+	)
 }
 
 func loadAst(schemaPath string) *ast.Root {
 	f, e := os.Open(schemaPath)
 	if e != nil {
-		log.Fatal(e)
+		log.Fatalf("error occured while loading schema: %v", e)
 	}
 	defer f.Close()
 	return parser.NewParser(bufio.NewReader(f)).Parse()
